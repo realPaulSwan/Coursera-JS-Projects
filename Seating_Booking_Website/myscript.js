b0905bd3d7164b507e26fd9c092a97b3dd676f00
realPaulSwan <paul.swan@wsu.edu>
2022-09-21 15:53:49 -0700

    Updated Seat Placing
    Updated Seating and added some classwork

--- Seating_Booking_Website/myscript.js
+++ Seating_Booking_Website/myscript.js
@@ -1,37 +1,37 @@
-const rows = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"];
+const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
 
 let html = "";
 let counter = 1;
 
 var reservedSeats = {
-	record1: {
-		seat: "b19",
-		owner: {
-			fname: "Joe",
-			lname: "Smith"
-		}
-	},
-	record2: {
-		seat: "b20",
-		owner: {
-			fname: "Joe",
-			lname: "Smith"
-		}
-	},
-	record3: {
-		seat: "b21",
-		owner: {
-			fname: "Joe",
-			lname: "Smith"
-		}
-	},
-	record4: {
-		seat: "b22",
-		owner: {
-			fname: "Joe",
-			lname: "Smith"
-		}
-	}
+    record1: {
+        seat: "b19",
+        owner: {
+            fname: "Joe",
+            lname: "Smith"
+        }
+    },
+    record2: {
+        seat: "b20",
+        owner: {
+            fname: "Joe",
+            lname: "Smith"
+        }
+    },
+    record3: {
+        seat: "b21",
+        owner: {
+            fname: "Joe",
+            lname: "Smith"
+        }
+    },
+    record4: {
+        seat: "b22",
+        owner: {
+            fname: "Joe",
+            lname: "Smith"
+        }
+    }
 };
 
 //------------ old---------------//
@@ -87,39 +87,39 @@ rows.forEach(function(row){
 document.getElementById('middle').innerHTML = html;
 */
 
-function makerows(sectionLength,rowLength,placement){
-    const rows = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"];
+function makerows(sectionLength, rowLength, placement) {
+    const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
 
     let html = "";
     let counter = 1;
 
-    rows.forEach(row =>{
-        switch(placement){
+    rows.forEach(row => {
+        switch (placement) {
             case "left": html += `<div class = "label">${row}</div>`; break;
             case "right": counter = counter + (rowLength - sectionLength); break;
-            default: counter = counter + ((rowLength - sectionLength)/2); break;
+            default: counter = counter + ((rowLength - sectionLength) / 2); break;
         }
         //loop here
-        for(let i=0; i<sectionLength;i++){
+        for (let i = 0; i < sectionLength; i++) {
             html += `<div class="a" id="${row + counter}">${counter}</div>`;
             counter++;
         }
 
-        switch (placement){
+        switch (placement) {
             case "left": counter = counter + (rowLength - sectionLength); break;
             case "right": html += `<div class = "label">${row}</div>`; break;
-            default: counter = counter + ((rowLength - sectionLength)/2); break;
+            default: counter = counter + ((rowLength - sectionLength) / 2); break;
         }
     });
-  
-  document.getElementById(placement).innerHTML = html;
+
+    document.getElementById(placement).innerHTML = html;
 
 }
-makerows(3,15,'left');
-makerows(3,15,'right');
-makerows(9,15,'middle');
+makerows(3, 15, 'left');
+makerows(3, 15, 'right');
+makerows(9, 15, 'middle');
 
-(function(){
+(function () {
     "use strict";
 
     var selectedSeats = [];
@@ -127,40 +127,88 @@ makerows(9,15,'middle');
 }())
 
 
-(function(){
-    "use strict";
-for(const key in reservedSeats){
+    (function () {
+        "use strict";
+        for (const key in reservedSeats) {
 
 
-    if(reservedSeats.hasOwnProperty(key)){
+            if (reservedSeats.hasOwnProperty(key)) {
 
-        const obj = reservedSeats[key];
+                const obj = reservedSeats[key];
 
-        document.getElementById(obj.seat).className = "r";
-        document.getElementById(obj.seat).className = "R";
-    }
+                document.getElementById(obj.seat).className = "r";
+                document.getElementById(obj.seat).className = "R";
+            }
 
 
-}
-}());
+        }
+    }());
 
-(function(){
+(function () {
 
     "use strict";
 
     var selectedSeats = [];
     var seats = document.querySelectorAll('.a');
 
-    seats.forEach(seat =>{
+    seats.forEach(seat => {
 
-        seat.addEventListener('click',function(event){
+        seat.addEventListener('click', function (event) {
+
+            seatSelectionProcess(seat.id)
 
 
         });
     });
 
-    function seatSelectionProcess(thisSeat){
+    function seatSelectionProcess(thisSeat) {
+
+        var index = selectedSeats.indexOf(thisSeat);
+
+        if (index > -1) {
+            selectedSeats.splice(index, 1);
+            document.getElementById(thisSeat).className = "a";
+        } else {
+            selectedSeats.push(thisSeat);
+            document.getElementById(thisSeat).className = "s";
+        }
+
 
     }
+
+    document.getElementById('reserve').addEventListener('click', function (event) {
+
+        event.preventDefault();
+        document.getElementById('resform').style.display = 'block';
+    });
+
+    document.getElementById('cancel').addEventListener('click', function (event) {
+
+        event.preventDefault();
+        document.getElementById('resform').style.display = 'none';
+    })
+
+    function manageConfirmForm() {
+        if (selectedSeats.length > 0) {
+            document.getElementById('confirmres').style.display = 'block';
+
+            let seatString = selectedSeats.toString();
+
+
+            document.getElementById('selectedseats').innerHTML = 'you have selected some seats ${seatString}';
+        } else {
+            document.getElementById('confirmres').style.display = 'none';
+
+            document.getElementById("selectedSeats").innerHTML = 'You need to select some seats to reserve. <br><a href="#" id ="error">Close</a> this dialog box and pick at least one seat';
+
+            document.getElementById('error').addEventListener('click', function () {
+                document.getElementById('confirmres').style.display = 'none';
+            });
+        }
+    }
+    manageConfirmForm();
+
+
+
 
 }());
